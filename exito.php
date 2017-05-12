ca8ad79a8b4572c5c72b8c7cb51617465203acc0
Arbust0 <Arbust908@gmail.com>
2017-05-12 18:30:59 -0300

    Update
    -En funciones agregue las funciones finales... no estan enganchadas a nada util, pero son lo qu enecesitamos.
    -Arme la pagina exito para que cuando te anotes te mande ahi... copado lo se.

---
+++ exito.php
@@ -0,0 +1,48 @@
+<?php
+$pagName = "Home";
+$userName = "Fede";
+require_once("head.php");
+require_once("nav.php"); ?>
+
+			<!-- Banner -->
+				<section id="banner">
+
+				</section>
+
+			<!-- Main -->
+				<section id="main" class="container">
+
+						<div class="row">
+
+							<div class="6u 12u(narrower)">
+
+								<section class="box special">
+									<h3>Te Registraste Exitosamentr!</h3>
+									<p>De ahora en adelante podes recibir Parches como este <span class="fa-stack">
+										<i class="fa fa-dot-circle-o fa-stack-1x"></i>
+										<i class="fa fa-sun-o fa-spin fa-fw fa-stack-2x"></i>
+									</span>
+								y vas a poder seguir las aventuras desde donde las dejaste!</p>
+									<hr>
+									<div class="row uniform 50%">
+										<div class="6u 12u(mobilep)">
+											<ul class="actions">
+												<li><a href="index.php" class="button fit ">Volver al Home</a></li>
+											</ul>
+										</div>
+										<div class="6u 12u(mobilep)">
+											<ul class="actions">
+												<li><a href="user.php" class="button fit">Ir a mi Perfil</a></li>
+											</ul>
+										</div>
+								</section>
+
+							</div>
+						</div>
+					</section>
+
+
+
+				</section>
+				
+<?php require_once("footer.php"); ?>
