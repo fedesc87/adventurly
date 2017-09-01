0922088d529b911d41d77f3e4fd7984187eab34a
Arbust0 <Arbust908@gmail.com>
2017-09-01 11:00:49 -0300

    Preping for LIve server
    -Added Things
    -???
    -Profit

--- Laravel/Adv_DB.sql
+++ Laravel/Adv_DB.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `adventurly` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `adventurly`;
 -- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
 --
 -- Host: localhost    Database: adventurly
